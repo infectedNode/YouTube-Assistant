932a43cc49a2a126c474eb560c20fb5ffeca2758
Shivam Sharma <shivam231198@gmail.com>
2019-06-11 17:22:55 +0530

    adding surface options to other intents

--- server/server.js
+++ server/server.js
@@ -147,9 +147,7 @@ agent.intent('Get Signin', (conv, params, signin) => {
       picture: `${payload.picture}`,
       token: null
     };
-    return db.collection('users').doc(`${payload.email}`).set(data).then((res) => {
-      conv.ask('I got your account details.  \nNow one last step left.  \nTo get authorised from youtube');
-      
+    return db.collection('users').doc(`${payload.email}`).set(data).then((res) => {      
       //Create url for the given email
       let token = {
         email: `${payload.email}`
@@ -164,13 +162,34 @@ agent.intent('Get Signin', (conv, params, signin) => {
         state: `${state}`
       });
 
-      conv.ask(new BasicCard({
-        text:'In order to give me access to **Read** your Youtube data',
-        buttons: new Button({
-          title: 'Go to this link ...',
-          url: `${url}`
-        })
-      })); 
+      conv.data.url = `${url}`;
+
+      let hasScreen = conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
+      let hasWebBrowser = conv.surface.capabilities.has('actions.capability.WEB_BROWSER');
+
+      let screenAvailable = conv.available.surfaces.capabilities.has('actions.capability.SCREEN_OUTPUT');
+      let browserAvailable = conv.available.surfaces.capabilities.has('actions.capability.WEB_BROWSER');
+
+      if(hasScreen && hasWebBrowser) {
+        conv.ask('I got your account details.  \nNow one last step left.  \nTo get authorised from youtube');
+        conv.ask('Please go to the following link, in order to continue with me.');
+        conv.ask(new BasicCard({
+          text:'In order to give me access to **Read** your Youtube data',
+          buttons: new Button({
+            title: 'Go to this link ...',
+            url: `${url}`
+          })
+        }));
+      } else if(screenAvailable && browserAvailable) {
+        let context = `I got your account details.  \nNow one last step left.  \nTo get authorised from youtube  \nAs you don\'t have a Web browser on this device.  \nTo provide you a YouTube Access link`;
+        let notification = 'YouTube Access Link';
+        let capabilities = ['actions.capability.WEB_BROWSER','actions.capability.SCREEN_OUTPUT'];
+        conv.ask(new NewSurface({context, notification, capabilities}));
+      } else {
+        // send link via email
+        conv.ask(`I got your account details.  \nNow one last step left.  \nTo get authorised from youtube.  \nAs you don\'t have a Web browser on this device. So I have mailed you the link.`);
+        conv.close('Please go to that link and give me an access to Read your Youtube data, in order to continue with me.');
+      } 
     }).catch((err) => {
       conv.close('Sorry, some error occured, please try again later');
     });

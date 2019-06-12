30cf1182291c9b37cfe304b3821f8b4dab861777
Shivam Sharma <shivam231198@gmail.com>
2019-06-13 02:28:15 +0530

    video intent iomprovement

--- server/server.js
+++ server/server.js
@@ -446,14 +446,14 @@ agent.intent('video', (conv) => {
           }          
           let date = moment(data.snippet.publishedAt).format("Do MMM YYYY");
           let title = data.snippet.title;
-          let thumbnail;
-          if(data.snippet.thumbnails.maxres.url) {
-            thumbnail = data.snippet.thumbnails.maxres.url;
-          } else if(data.snippet.thumbnails.standard.url) {
-            thumbnail = data.snippet.thumbnails.standard.url;
-          } else {
-            thumbnail = data.snippet.thumbnails.high.url; 
-          }
+          let thumbnail = data.snippet.thumbnails.high.url;
+          // if(data.snippet.thumbnails.maxres.url) {
+          //   thumbnail = data.snippet.thumbnails.maxres.url;
+          // } else if(data.snippet.thumbnails.standard.url) {
+          //   thumbnail = data.snippet.thumbnails.standard.url;
+          // } else {
+          //   thumbnail = data.snippet.thumbnails.high.url; 
+          // }
           let videoId = data.snippet.resourceId.videoId;
           return service.videos.list({
             auth: oauth2Client,

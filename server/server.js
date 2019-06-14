f6767add821de3aaf6cdd5d7a1d2d1ab7bf2aaa4
Shivam Sharma <shivam231198@gmail.com>
2019-06-14 16:15:45 +0530

    applying ssml

--- server/server.js
+++ server/server.js
@@ -64,17 +64,8 @@ function formatNumber(num) {
 
 agent.intent('Default Welcome Intent', (conv) => {
   if(conv.user.last.seen) {      //First time user's
-    conv.ask('<speak>' + 'Hi, welcome to your <emphasis level="moderate">YouTube Assistant.</emphasis>  \nI can provide latest updates<break time="200ms"/> about your YouTube channel<break time="300ms"/> or about your last video uploaded.  \nFor a demo, let say I have a YouTube channel<break time="100ms"/> "shivurocks".  \nAnd I want updates about my last video uploaded. So this is what I get.' + '</speak>');
-    conv.ask(`
-      <speak>
-        Your video "<emphasis level="moderate">Title of the Video</emphasis>" has got <break time="200ms"/> :-
-        10,000 Views <break time="300ms"/>,
-        5,000 Likes <break time="300ms"/>,
-        2,000 Comments <break time="300ms"/> and,
-        50 Dislikes <break time="500ms"/>. 
-      In order to get connected please say Sign In 
-      </speak>
-    `);
+    conv.ask('Hi, welcome to your YouTube Assistant.  \nI can provide latest updates about your YouTube channel or about your last video uploaded.  \nFor a demo, let say I have a YouTube channel "shivurocks".  \nAnd I want updates about my last video uploaded. So this is what I get.');
+    conv.ask('Your video  "Title of the Video" has got :-  \n10,000 views  \n5,000 likes  \n2,000 comments and  \n50 dislikes.  \nIn order to get connected please say Sign In');
     conv.ask(new BasicCard({
       image: new Image({
         url: `https://images.pexels.com/photos/2376994/pexels-photo-2376994.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=853&w=1280`,
